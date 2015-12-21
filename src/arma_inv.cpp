c2c2ac2278bfab3d385b2cb36fe13be6e22f23fb
helske <jouni.helske@jyu.fi>
2015-12-21 11:07:05 +0200

    added comments

--- src/arma_inv.cpp
+++ src/arma_inv.cpp
@@ -1,17 +1,19 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
-//
-using namespace Rcpp;
-// [[Rcpp::export]]
 
+using namespace Rcpp;
+
+// [[Rcpp::export]]
 NumericMatrix inv(const arma::mat& X) {
+  // this works fine
   arma::mat res = arma::inv(X);
   return wrap(res);
 }
 
-// [[Rcpp::export]]
 
+// [[Rcpp::export]]
 NumericMatrix inv_sympd(const arma::mat& X) {
+  // this gives errors in valgrind
   arma::mat res = arma::inv_sympd(X);
   return wrap(res);
 }
