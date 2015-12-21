b6abd53547701892d612fc94bcab0731966ca69c
helske <jouni.helske@jyu.fi>
2015-12-21 11:08:46 +0200

    use both methods in valgrind

--- valg.R
+++ valg.R
@@ -1,6 +1,11 @@
 library(debugpkg)
 set.seed(123)
 X <- crossprod(matrix(rnorm(25),5,5))
+
+# this works fine
 armainv(X)
+
+# this works seeminly fine but valgrind gives an error
+armainv_pd(X)
 
 # R -d "valgrind --tool=memcheck --leak-check=full" --vanilla < valg.R
